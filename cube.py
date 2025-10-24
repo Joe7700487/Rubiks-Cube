0dfd8f6dfd144a871f8b93e49fe48342a900ede9
Joe Petrash <w0500154@nscc.ca>
2025-10-23 23:09:09 -0300

    Draw state
    added function to draw a cubes state

--- cube.py
+++ cube.py
@@ -8,7 +8,51 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+#------------------------ Cube setup ---------------------------------
+
+size = 50
+globalOffset = 100
+uOffset = (size * 3 + globalOffset, size * 0 + globalOffset)
+lOffset = (size * 0 + globalOffset, size * 3 + globalOffset)
+fOffset = (size * 3 + globalOffset, size * 3 + globalOffset)
+rOffset = (size * 6 + globalOffset, size * 3 + globalOffset)
+bOffset = (size * 9 + globalOffset, size * 3 + globalOffset)
+dOffset = (size * 3 + globalOffset, size * 6 + globalOffset)
+blue    = (0, 0, 255)
+green   = (0, 255, 0)
+red     = (255, 0, 0)
+white   = (255, 255, 255)
+orange  = (255,165,0)
+yellow  = (255,215,0)
+
+solvedCube = "UUUUUUUUULLLLLLLLLFFFFFFFFFRRRRRRRRRBBBBBBBBBDDDDDDDDD"
+turnedCube = "UUUUUUUUUFFFLLLLLLRRRFFFFFFBBBRRRRRRLLLBBBBBBDDDDDDDDD"
+
+# take a string to display the state of the cube
+def drawState (state):
+    xOffset = 0
+    yOffset = 0
+    offset = uOffset
+    color = ()
+    for i in range(len(state)):
+        sticker = state[i]
+        if   i > 44: offset = dOffset
+        elif i > 35: offset = bOffset
+        elif i > 26: offset = rOffset
+        elif i > 17: offset = fOffset
+        elif i > 8: offset = lOffset
+        if   sticker == "U": color = white
+        elif sticker == "L": color = orange
+        elif sticker == "F": color = green
+        elif sticker == "R": color = red
+        elif sticker == "B": color = blue
+        elif sticker == "D": color = yellow
+        xOffset = (i % 3) * size
+        if (i % 3 == 0) and (i > 0): yOffset += size
+        if i % 9 == 0: yOffset = 0
+        print(offset, color, i, i % 9, yOffset)
+        pygame.draw.rect(screen, color,  (offset[0] + xOffset, offset[1] + yOffset, size, size), 0)
+        pygame.draw.rect(screen, "black",  (offset[0] + xOffset, offset[1] + yOffset, size, size), 5)
 
 while running:
     # poll for events
@@ -18,20 +62,11 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("black")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
+    # drawState(solvedCube)
+    drawState(turnedCube)
+    
     # flip() the display to put your work on screen
     pygame.display.flip()
 
